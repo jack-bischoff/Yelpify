82bb1d3d59d01ec363dc20a3e32a6630bef8589b
Jack Bischoff <jackbischoff12@gmail.com>
2017-06-15 13:00:18 -0400

    added ref to search bar

--- src/components/PlaceContent.js
+++ src/components/PlaceContent.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Place from './Place';
 
 class PlaceContent extends React.Component {
-    
+
     render() {
       return (
         <div>
           <div className="headerColor uk-light uk-padding-small">
             <h2 className="uk-padding-small uk-margin-remove">Yelpify</h2>
             <form className="uk-form">
-                <input type="text" className="uk-input uk-margin-small-top little-round" placeholder="Search" />
+                <input
+                  type="text"
+                  className="uk-input uk-margin-small-top little-round"
+                  placeholder="Search"
+                  ref={(input) => { this.textInput = input; }}
+                />
             </form>
           </div>
           <div >
