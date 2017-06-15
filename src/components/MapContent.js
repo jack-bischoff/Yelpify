d642687746e7a7b768eabea091e4face78f60f77
Jack Bischoff <jackbischoff12@gmail.com>
2017-06-15 12:59:27 -0400

    Added extra places

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import PlaceContent from './PlaceContent';
 import GoogleMapReact from 'google-map-react';
 import CurrentLocationMarker from './currentLocationMarker';
 import PlaceContent from './PlaceContent';
@@ -14,7 +13,11 @@ class MapContent extends React.Component {
 
   }
 
+  _loadPlaces(){
+  }
+
   componentWillReceiveProps(){
+    //this._loadPlaces();
     this.setState({
       ready: true
     });
@@ -23,28 +26,29 @@ class MapContent extends React.Component {
   render() {
 
     const data = [
-      {name: "Restaurant"},
-      {name: "Delicious Restaurant"},
-      {name: "Extraordinary Restaurant"},
-      {name: "Fabulous Restaurant"},
-      {name: "Worse than a 7-11 bathroom"},
-      {name: "Restaurant"},
-      {name: "Delicious Restaurant"},
-      {name: "Extraordinary Restaurant"},
-      {name: "Fabulous Restaurant"},
-      {name: "Worse than a 7-11 bathroom"},
-      {name: "Restaurant"},
-      {name: "Delicious Restaurant"},
-      {name: "Extraordinary Restaurant"},
-      {name: "Fabulous Restaurant"},
-      {name: "Worse than a 7-11 bathroom"}
+      {name: 'Restaurant'},
+      {name: 'Delicious Restaurant'},
+      {name: 'Extraordinary Restaurant'},
+      {name: 'Fabulous Restaurant'},
+      {name: 'Worse than a 7-11 bathroom'},
+      {name: 'Restaurant'},
+      {name: 'Delicious Restaurant'},
+      {name: 'Extraordinary Restaurant'},
+      {name: 'Fabulous Restaurant'},
+      {name: 'Worse than a 7-11 bathroom'},
+      {name: 'Restaurant'},
+      {name: 'Delicious Restaurant'},
+      {name: 'Extraordinary Restaurant'},
+      {name: 'Fabulous Restaurant'},
+      {name: 'Worse than a 7-11 bathroom'}
     ]
+    const places = [];
 
     return (this.state.ready) ?
     (
       <div>
         <div data-uk-grid data-uk-height-viewport>
-          <div className="uk-width-3-4 ">
+          <div className='uk-width-3-4 '>
             <GoogleMapReact
               center={this.props.location}
               zoom={18}
@@ -52,9 +56,9 @@ class MapContent extends React.Component {
               <CurrentLocationMarker {...this.props.location} />
             </GoogleMapReact>
           </div>
-          
-          <div className="uk-width-1-4 uk-padding-remove uk-box-shadow-large z-low">
-            <div className="uk-section uk-padding-remove uk-height-1-1">
+
+          <div className='uk-width-1-4 uk-padding-remove uk-box-shadow-large z-low'>
+            <div className='uk-section uk-padding-remove uk-height-1-1'>
               <PlaceContent places={data} />
             </div>
           </div>
