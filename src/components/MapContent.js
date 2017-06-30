d17f2822aa5d7bd3fe1a842d5702701cad5c92be
Jack Bischoff <jackbischoff12@gmail.com>
2017-06-30 14:49:04 -0400

    Lifted State and created a better presentational/functional split.

--- src/components/MapContent.js
+++ src/components/MapContent.js
@@ -1,99 +1,58 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import CurrentLocationMarker from './currentLocationMarker';
-import PlaceContent from './PlaceContent';
+
+import PlaceList from './Placelist';
+import MapDisplay from './MapDisplay';
 
 
+export default class MapContent extends React.Component {
 
-class MapContent extends React.Component {
-  constructor(props) {
+  constructor(props){
     super(props)
+    this._setAPI = this._setAPI.bind(this);
+    this._updateMarkers = this._updateMarkers.bind(this);
   }
+
   state = {
-    ready: false,
-    map: {}
+    api: [],
+    markers: [],
+    ready: false
   }
 
-  componentWillReceiveProps(nextProps){
-    const location = nextProps.location;
-    const urlParams = {
-      key: ' AIzaSyDLpwXrHJQf_aXsMdkiVPwtV1JKPLBz-8w',
-      libraries: 'places'
-    }
 
-
-    const map = 
-    <GoogleMapReact
-      bootstrapURLKeys={urlParams}
-      yesIWantToUseGoogleMapApiInternals={true}
-      center={location}
-      zoom={18}
-      onGoogleApiLoaded={ 
-        ({map, maps}) => {
-          let request = {
-            location: {...location},
-            radius: 1000,
-            type: ['restaurant']
-          }
-
-          let service = new maps.places.PlacesService(map);
-          service.nearbySearch(request, (results, status) => {
-            if (status === maps.places.PlacesServiceStatus.OK) {
-              results.map( (place) => {
-                console.log(place.name);
-              })
-            }
-          });
-        }
-      }
-    >
-      <CurrentLocationMarker {...location} />
-    </GoogleMapReact>
-    
-
+  _setAPI(map, maps) {
     this.setState({
-      ready: true,
-      map: map
+      api: [map, maps]
     });
   }
 
-  componentDidMount(){
-    
+  _updateMarkers(newMarkers) {
+    this.setState({
+      markers: newMarkers
+    });
   }
 
+  componentWillReceiveProps(nextProps) {
+    let usersLocationMarker = [{location: nextProps.location, type: 'user'}]
+
+    this.setState({
+      ready: true,
+      markers: usersLocationMarker
+    })
+  }
 
   render() {
-
-    const data = [
-      {name: 'Restaurant'},
-      {name: 'Delicious Restaurant'},
-      {name: 'Extraordinary Restaurant'},
-      {name: 'Fabulous Restaurant'},
-      {name: 'Worse than a 7-11 bathroom'},
-      {name: 'Restaurant'},
-      {name: 'Delicious Restaurant'},
-      {name: 'Extraordinary Restaurant'},
-      {name: 'Fabulous Restaurant'},
-      {name: 'Worse than a 7-11 bathroom'},
-      {name: 'Restaurant'},
-      {name: 'Delicious Restaurant'},
-      {name: 'Extraordinary Restaurant'},
-      {name: 'Fabulous Restaurant'},
-      {name: 'Worse than a 7-11 bathroom'}
-    ]
-    const places = [];
-
+    console.log(this.state)
     return (this.state.ready) ?
     (
       <div>
         <div data-uk-grid data-uk-height-viewport>
-          <div className='uk-width-3-4 '>
-            {this.state.map}
+          <div className='uk-width-3-4'>
+            <MapDisplay location={this.props.location} markers={this.state.markers} apiLoaded={this._setAPI}/>
           </div>
 
           <div className='uk-width-1-4 uk-padding-remove uk-box-shadow-large z-low'>
             <div className='uk-section uk-padding-remove uk-height-1-1'>
-              <PlaceContent places={data} />
+              <PlaceList location={this.props.location} api={this.state.api} updateMarkers={this._updateMarkers} />
             </div>
           </div>
         </div>
@@ -102,5 +61,3 @@ class MapContent extends React.Component {
     : <h1>Loading Map...</h1>
   }
 }
-
-export default MapContent;
