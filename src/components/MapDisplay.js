5368e3e27bf21d7f300ad4b590073295000c6ea8
Jack Bischoff <jackbischoff12@gmail.com>
2017-06-30 14:51:57 -0400

    Created a new presentational component which handles displaying the map.

---
+++ src/components/MapDisplay.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import LocationMarker from './LocationMarker';
+import GoogleMapReact from 'google-map-react';
+require('../styles/App.css');
+
+const urlParams = {
+  key: ' AIzaSyDLpwXrHJQf_aXsMdkiVPwtV1JKPLBz-8w',
+  libraries: 'places'
+}
+
+export default class MapDisplay extends React.Component {
+
+  render() {
+
+    return (
+      <GoogleMapReact
+        bootstrapURLKeys={urlParams}
+        center={this.props.location}
+        zoom={18}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={
+          ({map, maps}) => {
+            this.props.apiLoaded(map, maps);
+          }
+        }
+      >
+        <LocationMarker {...this.props.location} icon={'user'} />
+        {
+          this.props.markers.map( (marker) => {
+            return <LocationMarker {...marker.location} key={marker.place_id} />
+          })
+        }
+      </GoogleMapReact>
+    )
+  }
+
+}
