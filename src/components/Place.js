4029803f6ea043de7d71bd11af2beb586066723b
Jack Bischoff <jackbischoff12@gmail.com>
2017-07-05 11:44:57 -0400

    Added new rating icon

--- src/components/Place.js
+++ src/components/Place.js
@@ -4,8 +4,18 @@ class Place extends React.Component {
 
   render() {
     return (
-      <div>
-        <h5>{this.props.name}</h5>
+      <div data-uk-grid>
+        <div className="uk-width-4-5">
+          <h5>{this.props.name}</h5>
+        </div>
+        <div className="uk-width-1-5">
+          <div className="uk-text-center">
+            <p className="uk-text-smallest uk-margin-remove">{this.props.rating}</p>
+            <div className="starFormat">
+              <span data-uk-icon="icon: star; ratio: 0.8"></span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
