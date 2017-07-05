ff58248a1fc5060df7f4f0e486b8669d180e0e8f
Jack Bischoff <jackbischoff12@gmail.com>
2017-07-05 15:49:30 -0400

    Removed uikit Icons code. No longer using it.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
+
 
 import App from './components/Main';
-
-// loads the Icon plugin
-UIkit.use(Icons);
 
 
 // Render the main component into the dom
